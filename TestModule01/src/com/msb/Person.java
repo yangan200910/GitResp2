917fcd1ddd35b8cb903560d0bc8daf8bb6b60fac
shouwangzhe <zscd@113456.com>
2022-05-06 23:17:36 +0800

    注释

--- TestModule01/src/com/msb/Person.java
+++
@@ -1,7 +0,0 @@
-package com.msb;
-
-public class Person {
-    private int age;
-    private String name;
-	private double height;
-}
--- TestModule01/src/com/msb/Student.java
+++
@@ -1,8 +0,0 @@
-package com.msb;
-
-public class Student {
-    private int age;
-    private String name;
-    private double weight;
-	private double height;
-}
